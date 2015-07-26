e1ce400dd685f0929590513935fccba3b14c9311
pgergo <pgege42@gmail.com>
2015-07-26 22:59:27 +0200

    templates update

--- templates/init.php
+++ templates/init.php
@@ -5,8 +5,9 @@
  * Initalize Tesseract by checking some settings.
  */
 
-namespace Tesseract;
-
 if (!function_exists('kint') && php_sapi_name() !== 'cli') {
   drupal_set_message('Devel Kint is not enabled. It is recommended to turn it on for Development environments.', 'warning');
 }
+
+// Global constants.
+define('ROOT', Drupal::root());
